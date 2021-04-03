f84cd534e8e537de6c1134068e54ede40164b2d5
adambourhim <benbourhim7@gmail.com>
2021-04-03 12:18:28 +0300

    second comit

--- src/App.js
+++ src/App.js
@@ -6,13 +6,19 @@ function App() {
       <h3>Reminders</h3>
       <br />
       <h3>Add Reminder</h3>
-      <form>
+      <form className="container">
         <label htmlFor="id">Id</label>
+        <br />
         <input name="id" placeholder="Id" />
+        <br />
         <label htmlFor="reminder">Reminder</label>
+        <br />
         <input name="reminder" placeholder="Reminder" />
+        <br />
         <label htmlFor="time">Time</label>
+        <br />
         <input name="time" placeholder="Time" />
+        <br />
         <button type="submit">Add</button>
       </form>
     </div>
